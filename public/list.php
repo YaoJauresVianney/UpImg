152dea2d704561ea6736e8ac3ebe39a566a853ea
JauresVianney <yaojauresvianney@gmail.com>
2019-05-28 10:46:21 +0000

    List.php modified

--- public/list.php
+++ public/list.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Your gallery</h1>
 <?php
 
 $pdo = new PDO('mysql:host=localhost;dbname=upimg', 'root', '');
@@ -12,3 +22,5 @@ foreach($a as $e){
     <?php
 }
 ?>
+</body>
+</html>
